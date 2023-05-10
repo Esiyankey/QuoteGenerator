9d06b48ba4826cc032d4db7cb4d3f322e13468db
Esi Yankey <graceyan244@gmail.com>
2023-05-10 19:52:52 +0000

    feat:feature a loading Bar

---
+++ src/LoadingBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './App.css'
+export const LoadingBar = () => {
+  return (
+    <div class="spinner"></div>
+ 
+  )
+}
--- src/Quote.jsx
+++ src/Quote.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { LoadingBar } from "./LoadingBar";
 import { TiArrowBack } from "react-icons/ti";
 export const Quote = () => {
   const [getQuote, setGetQuote] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const fetchQuote = async () => {
     const response = await fetch("https://type.fit/api/quotes ");
@@ -12,20 +14,26 @@ export const Quote = () => {
     setGetQuote(randomQuote);
     console.log(data);
   };
+  const handleClick = async () => {
+    setLoading(true);
+    await fetchQuote();
+    setLoading(false);
+  };
 
   return (
     <div className="main-container">
       <div className="quote">
         <h1>
-          "{getQuote.text}"<br/>
+          "{getQuote.text}"<br />
           <span>Author:{getQuote.author}</span>
         </h1>
       </div>
       <div className="foot">
         {/* <TiArrowBack className="icon" /> */}
-        <button className="btn" onClick={fetchQuote}>
+        <button className="btn" onClick={handleClick}>
           Get me a Quote
         </button>
+        {loading && <LoadingBar />}
       </div>
     </div>
   );
